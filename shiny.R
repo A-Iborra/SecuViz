7fc644fa44e85bd0c973b37271386bf2061c1302
A-Iborra <alex.iborra@orange.fr>
2021-03-08 15:43:44 +0100

    der version

--- shiny.R
+++ shiny.R
@@ -1,17 +1,17 @@
 ### Chargement des librairies
 
+library(visNetwork)
+library(visTree)
 library(shiny)
+library(rpart)
 library(readxl)
 library(dplyr)
 library(ggplot2)
 library(shinydashboard)
-anyLib::anyLib(c("shiny", "shinydashboard", "shinyWidgets", "DT", "plotly", "ggplot2", "googleVis", "colourpicker"))
-#install.packages("chorddiag")
-#devtools::install_github("mattflor/chorddiag")
-#library(chorddiag)
+anyLib::anyLib(c("shiny", "shinydashboard", "shinyWidgets", "DT", "plotly", "ggplot2", "colourpicker"))
 
 ### Chargement et manipulation des données
-#data <- read.table("Jeu-1-projet.csv",sep=",")
+#data <- read.table("Jeu-1-projet.csv",sep=",",header = T)
 #colnames(data) <- c("ipsrc","ipdst","portdst","proto","action","date","regle")
 
 ### Définition des éléments de l'interface
@@ -21,8 +21,9 @@ ui <- dashboardPage(
     sidebarMenu(id = "tabs",
                 menuItem("Home", tabName = "PagePrinc", icon = icon("home")),
                 menuItem("Analyse des Flux", tabName = "flux", icon = icon("poll")),
-                menuItem("CAH", tabName = "clustering", icon = icon("project-diagram"))
-    )
+                menuItem("CAH", tabName = "clustering", icon = icon("project-diagram")),
+                menuItem("Arbre de décision", tabName = "arbre", icon = icon("tree")))
+  
   ),
   dashboardBody(
     tabItems(
@@ -44,7 +45,28 @@ ui <- dashboardPage(
               h1("TOP 10/5")
               ),
       tabItem(tabName = "clustering",
-              h1("CAH")
+              h1("CAH"),
+              sliderInput("clusters", "Nombre de cluster:", 
+                          min = 2,        # 1 cluster is pointless
+                          max = 10,       # too many is too crowded
+                          value = 4),    # sensible start
+              # radioButtons("proto", "Protocole :",
+              #              c("TCP" = "tcp",
+              #                "UDP" = "udp",
+              #                "TCP & UDP" = "tcpudp")),
+              # radioButtons("ports", "Ports :",
+              #              c("Inférieur 1024" = "inf",
+              #                "Supérieur 1024" = "sup",
+              #                "Tous les ports" = "touslespo")),
+              plotlyOutput("distPlot"),
+              textOutput("test")
+      ),
+      tabItem(tabName = "arbre",
+              h1("Arbre de décision"),
+              varSelectInput("cible", "Variables à expliquer:", data, multiple = FALSE),
+              varSelectInput("expli", "Variables explicatives:", data, multiple = TRUE),
+              actionButton("go", "Valider"),
+              visTreeModuleUI(id = "id1", rpartParams = FALSE, visTreeParams = FALSE)
       )
     )
   )
@@ -61,8 +83,75 @@ server <- function(input, output, session){
     read.csv(infile$datapath, header = TRUE)
   })
   
+  observeEvent(
+    input$FileInput,
+    updateVarSelectInput(session, "cible", data = datasetInput()))
+  
+  observeEvent(
+    input$FileInput,
+    updateVarSelectInput(session, "expli", data = datasetInput()))  
+  
+
   output$table = DT::renderDataTable(datasetInput())
+  
+  
+  output$distPlot <- renderPlotly({
+    data_clus <- datasetInput()
+    
+    
+    data_clus[,c(1,2,4:6)] <- lapply(data_clus[,c(1,2,4:6)],as.factor)
+    gower_dist <- daisy(data_clus, metric = "gower")
+    gower_mat <- as.matrix(gower_dist)
+    k <- input$clusters
+    
+    pam_fit <- pam(gower_dist, diss = TRUE, k)
+    tsne_obj <- Rtsne(gower_dist, is_distance = TRUE)
+    
+    tsne_data <- tsne_obj$Y %>%
+      data.frame() %>%
+      setNames(c("X", "Y")) %>%
+      mutate(cluster = factor(pam_fit$clustering)) %>% 
+      mutate(ipsource = data_clus$ipsrc)
+    
+    ggplot(aes(x = X, y = Y), data = tsne_data) +
+      geom_point(aes(color = cluster))
+  })
+  
+  # output$test <- renderPrint({
+  #   data_2 <- datasetInput()
+  #   data_2[,c(1,2,4:6)] <- lapply(data_2[,c(1,2,4:6)],as.factor)
+  #   gower_dist2 <- daisy(data_2, metric = "gower")
+  #   gower_mat2 <- as.matrix(gower_dist2)
+  #   k2 <- input$clusters
+  #   
+  #   pam_fit2 <- pam(gower_dist2, diss = TRUE, k2)
+  #   pam_results2 <- data %>%
+  #     mutate(cluster = pam_fit2$clustering) %>%
+  #     group_by(cluster) %>%
+  #     do(the_summary = summary(.))
+  #   pam_results2$the_summary
+  #   
+    
+  #})
+  
+  observeEvent(input$go,{
+    data = datasetInput()
+    #var = list()
+    var = input$expli
+    cib = input$cible
+    #temp <- data[,input$expli[]]
+    var = unlist(as.character(input$expli))
+    temp <- data %>% select(var,cib)
+    print(cib)
+    #temp <- data[,input$expli,drop=FALSE]
+    res <- rpart(proto ~.,data=temp,method = "class", control = rpart.control(cp = 0.5, minsplit = 2),model =T)
+    shiny::callModule(visTreeModuleServer, "id1", data = shiny::reactive(res))
+  })
+  
+  
 }
+
+
 
 ### Lancement de l'application
 shinyApp(ui, server)
