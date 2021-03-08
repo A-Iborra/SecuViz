6f5923a142c539aa3079c39f49582bd3c5bac3b3
A-Iborra <alex.iborra@orange.fr>
2021-03-09 00:43:27 +0100

    mise en forme deuspi

--- shiny.R
+++ shiny.R
@@ -23,8 +23,8 @@ source("fonctions.R")
 
 
 ### Définition des éléments de l'interface
-ui <- dashboardPage(
-  dashboardHeader(title = "Sécurité et visualisation des données"),
+ui <- dashboardPage(skin = "black",
+  dashboardHeader(title = "Sécurité et visualisation des données",titleWidth = 450),
   dashboardSidebar(
     sidebarMenu(id = "tabs",
                 menuItem("Home", tabName = "PagePrinc", icon = icon("home")),
@@ -38,12 +38,21 @@ ui <- dashboardPage(
     tabItems(
       tabItem(tabName = "PagePrinc",
               h1("Description du Projet"),
+              p("Le projet 2021 aborde des problématiques classiques et surtout connues reposant généralement sur
+              la fouille de données et la détection des intrusions. Cette année, nous ajoutons une revue d’utilisation
+              des règles d’un Firewall généralement nécessaire pour une satisfaction normative. Nous intégrons
+              également une migration de ces règles vers un autre Firewall de même technologie. En résumé une
+              analyse de règles sera faite à partir du log de Firewall IPTABLES de type on cloud et une migration de
+              ces dernières sera à réaliser sur un Firewall de type IPTABLES stand alone. La partie fouille de données
+              sera à réaliser par la suite."),
+              
               h1("Lecture des données"),
+              p("Le chargement des données supporte uniquement des fichiers txt ou csv avec comme séparateur la ',' ou le ';'"),
               fileInput("FileInput",label = NULL,
                         buttonLabel = "Browse...",
                         placeholder = "No file selected"),
               
-              h1("Données"),
+              h1("Visualisation des données"),
               dataTableOutput('table')
       ),
       
@@ -54,7 +63,23 @@ ui <- dashboardPage(
               h1("TOP 10/5")
               ),
       tabItem(tabName = "clustering",
-              h1("t-SNE"),
+              h1("Réduction de dimension t-SNE"),
+              p("T-SNE : L’algorithme de réduction de dimensionnalité appelé t-distributed 
+              stochastic neighbor embedding (t-SNE) est un algorithme d’apprentissage non supervisé. 
+              Développé par Laurens van der Maaten et Geoffrey Hinton, il permet d’analyser des données 
+              décrites dans des espaces à forte dimensionnalité (via un grand nombre de descripteurs) 
+              pour les représenter dans des espaces à deux ou trois dimensions. Cet algorithme est très 
+              utilisé car il facilite la visualisation de données ayant beaucoup de descripteurs.
+              T-SNE est un algorithme non-linéaire de “feature extraction” qui construit une nouvelle 
+              représentation des données de telle sorte que les données proches dans l’espace 
+              original aient une probabilité élevée d’avoir des représentations proches dans le 
+              nouvel espace. A l’inverse, les données qui sont éloignées dans l’espace original, 
+              ont une probabilité faible d’avoir des représentations proches dans le nouvel espace. 
+              En pratique la similarité entre chaque paire de données, dans les deux espaces, est 
+              mesurée par le biais de calculs probabilistes basés sur des hypothèses de distribution. 
+              Et les nouvelles représentations se construisent de telle sorte à minimiser la 
+              différence entre les distributions de probabilités mesurées dans l’espace original et 
+              celles du nouvel espace."),
               sliderInput("clusters", "Nombre de cluster:", 
                           min = 2,        # 1 cluster is pointless
                           max = 10,       # too many is too crowded
@@ -64,6 +89,14 @@ ui <- dashboardPage(
       ),
       tabItem(tabName = "kmeans",
               h1("Visualisation des variables selon cluster Kmeans"),
+              p("K-means (ou K-moyennes) : C’est l’un des algorithmes de clustering les plus répandus.
+              Il permet d’analyser un jeu de données caractérisées par un ensemble de descripteurs, 
+              afin de regrouper les données “similaires” en groupes (ou clusters).La similarité entre 
+              deux données peut être inférée grâce à la “distance” séparant leurs descripteurs ; ainsi 
+              deux données très similaires sont deux données dont les descripteurs sont très proches. 
+              Cette définition permet de formuler le problème de partitionnement des données comme la 
+              recherche de K “données prototypes”, autour desquelles peuvent être regroupées les autres 
+              données."),
               sliderInput("clusters2", "Nombre de cluster:", 
                           min = 2,        # 1 cluster is pointless
                           max = 10,       # too many is too crowded
